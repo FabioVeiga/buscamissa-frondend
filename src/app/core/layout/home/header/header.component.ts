dbccba8af681f6e091f89b655f82b584f7a387e4
Marcus Vinicius <marcus.sayegh@joit.com.br>
2025-03-13 22:03:47 -0300

    correção botão header.

--- src/app/core/layout/home/header/header.component.ts
+++ src/app/core/layout/home/header/header.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header-home',
-  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatToolbarModule, MatButtonModule, MatIconModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
