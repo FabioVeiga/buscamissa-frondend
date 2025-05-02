f24b66e330da9cc1cc35ab81783638ac1d8aab59
Fabio Veiga <52764579+FabioVeiga@users.noreply.github.com>
2025-05-01 22:10:46 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,6 @@ export const environment = {
   config: {
     production: true,
     apiURL: "https://busca-missa.azurewebsites.net/api/",
-    token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRyb2lkYmluaG9AZ21haWwuY29tIiwicm9sZSI6IkFwcCIsIm5iZiI6MTc0NjE0Njc1NSwiZXhwIjoyMDYxNjc5NTU1LCJpYXQiOjE3NDYxNDY3NTV9.Pw1tADQXYEpu_Fz9Nkt-8gwS9v56-d2bekAXEHB4p5oeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InN1cG9ydGVAYnVzY2FybWlzc2EuY29tLmJyIiwicm9sZSI6IkFwcCIsIm5iZiI6MTczMDk0MjQ4NCwiZXhwIjoyMDQ2NDc1Mjg0LCJpYXQiOjE3MzA5NDI0ODR9.2UEel10ImLMm-kT2ROFMRp7lax46IZ1VPSkloNw9bqg",
+    token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRyb2lkYmluaG9AZ21haWwuY29tIiwicm9sZSI6IkFwcCIsIm5iZiI6MTc0NjE0ODE5NywiZXhwIjoyMDYxNjgwOTk3LCJpYXQiOjE3NDYxNDgxOTd9.TP3GPb6RVPphfJSn5PWnV7r16CoOZrvHIESO2X5X0lg",
   },
 };
