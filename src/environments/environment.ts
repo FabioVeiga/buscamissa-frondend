49130584362b510978bd4daba9cf34fb0baed258
Marcus Vinicius <marcus.sayegh@joit.com.br>
2025-05-03 16:11:27 -0300

    alteração enviroment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
     angular: `${applicationBase.angular} DEV`,
   },
   config: {
-    production: true,
+    production: false,
     apiURL: "https://busca-missa.azurewebsites.net/api/",
     token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRyb2lkYmluaG9AZ21haWwuY29tIiwicm9sZSI6IkFwcCIsIm5iZiI6MTc0NjE0ODE5NywiZXhwIjoyMDYxNjgwOTk3LCJpYXQiOjE3NDYxNDgxOTd9.TP3GPb6RVPphfJSn5PWnV7r16CoOZrvHIESO2X5X0lg",
   },
