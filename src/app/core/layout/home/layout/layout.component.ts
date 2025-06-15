4230d4269ed8ca162b8451edf17973e879faf04a
Marcus Vinicius <marcus.sayegh@joit.com.br>
2025-06-15 11:04:06 -0300

    alteração layout

--- src/app/core/layout/home/layout/layout.component.ts
+++ src/app/core/layout/home/layout/layout.component.ts
@@ -3,7 +3,10 @@ import { CommonModule } from "@angular/common";
 import { HeaderHomeComponent } from "../header/header.component";
 import { FooterHomeComponent } from "../footer/footer.component";
 import { RouterModule } from "@angular/router";
-
+declare interface Window {
+  adsbygoogle: any[];
+}
+declare var adsbygoogle: any[];
 @Component({
   selector: "app-layout-home",
   imports: [
@@ -14,7 +17,7 @@ import { RouterModule } from "@angular/router";
   ],
   template: `<div class="layout-wrapper container">
     <div class="layout-main-container">
-    <app-header-home></app-header-home>
+      <app-header-home></app-header-home>
       <div class="layout-main">
         <div class="card"><router-outlet></router-outlet></div>
       </div>
@@ -22,8 +25,40 @@ import { RouterModule } from "@angular/router";
     </div>
     <div class="layout-mask animate-fadein"></div>
   </div> `,
+  styles: [
+    `
+      .ads_resp {
+        width: 320px;
+        height: 100px;
+        margin: 0 auto;
+      }
+      @media (min-width: 500px) {
+        .ads_resp {
+          width: 480px;
+          height: 60px;
+        }
+      }
+      @media (min-width: 900px) {
+        .ads_resp {
+          width: 900px;
+          height: 90px;
+        }
+      }
+    `,
+  ],
 })
 export class LayoutHomeComponent {
-  patrocinioDireita = '';
-  patrocinioEsquerda = '';
+  patrocinioDireita = "";
+  patrocinioEsquerda = "";
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      try {
+        (window as any).adsbygoogle = (window as any).adsbygoogle || [];
+        (window as any).adsbygoogle.push({});
+      } catch (e) {
+        console.error("ads", e);
+      }
+    }, 900);
+  }
 }
