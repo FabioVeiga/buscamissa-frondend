3ee64648b0960d0fe57dbc81705fe5a8e7023ded
Marcus Vinicius <marcus.sayegh@joit.com.br>
2025-06-27 19:53:24 -0300

    alteração layout

--- src/app/core/layout/home/layout/layout.component.ts
+++ src/app/core/layout/home/layout/layout.component.ts
@@ -19,42 +19,12 @@ declare var adsbygoogle: any[];
     <div class="layout-main-container">
       <app-header-home></app-header-home>
       <div class="layout-main">
-        <div class="text-center">
-          <ins
-            class="adsbygoogle ads_resp"
-            style="display: inline-block"
-            data-ad-client="ca-pub-id"
-            data-ad-slot="ad-slot-id"
-            data-full-width-responsive="true"
-          ></ins>
-        </div>
         <div class="card"><router-outlet></router-outlet></div>
       </div>
       <app-footer-home></app-footer-home>
     </div>
     <div class="layout-mask animate-fadein"></div>
   </div> `,
-  styles: [
-    `
-      .ads_resp {
-        width: 320px;
-        height: 100px;
-        margin: 0 auto;
-      }
-      @media (min-width: 500px) {
-        .ads_resp {
-          width: 480px;
-          height: 60px;
-        }
-      }
-      @media (min-width: 900px) {
-        .ads_resp {
-          width: 900px;
-          height: 90px;
-        }
-      }
-    `,
-  ],
 })
 export class LayoutHomeComponent {
   patrocinioDireita = "";
